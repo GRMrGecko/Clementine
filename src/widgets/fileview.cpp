99bdad860a3f1308a640602f51d30400001defe4
Mr. Gecko <GRMrGecko@gmail.com>
2017-04-08 00:06:48 -0500

    Update fileview.cpp

--- src/widgets/fileview.cpp
+++ src/widgets/fileview.cpp
@@ -33,7 +33,7 @@ const char* FileView::kFileFilter =
     "*.mp3 *.ogg *.flac *.mpc *.m4a *.m4b *.aac *.wma "
     "*.mp4 *.spx *.wav *.m3u *.m3u8 *.pls *.xspf "
     "*.asx *.asxini *.cue *.ape *.wv *.mka *.opus "
-    "*.oga *.mka *.mp2";
+    "*.oga *.tta *.mp2 *.m4v *.m4r *.asf *.aif *.aiff";
 
 FileView::FileView(QWidget* parent)
     : QWidget(parent),
